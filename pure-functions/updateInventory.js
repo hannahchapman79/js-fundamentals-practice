eb02f028117083d9e1d8fd3aef5d2874f2190321
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-17 12:26:52 +0700

    Adds logic to updateInventory

--- pure-functions/updateInventory.js
+++ pure-functions/updateInventory.js
@@ -1,5 +1,19 @@
 function updateInventory(inventory, update) {
-    // My logic here
-  }
-  
-  module.exports = updateInventory;
+const itemExists = inventory.some((item) => item.name === update.name);
+
+if (itemExists) {
+    return inventory.map((item) => {
+        if (item.name === update.name) {
+            return {
+                name: item.name,
+                quantity: item.quantity + update.quantity
+            };
+        }
+        return item;
+    });
+} else {
+    return [...inventory, { name: update.name, quantity: update.quantity }];
+}
+}
+
+module.exports = updateInventory;
