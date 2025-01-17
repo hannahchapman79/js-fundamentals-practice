3fb01bc03c938372cab22a2340102dc9a8bcd7ca
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-17 11:41:08 +0700

    Adds logic for addTagToPosts

--- pure-functions/addTagToPosts.js
+++ pure-functions/addTagToPosts.js
@@ -1,5 +1,19 @@
+/* Description:
+Takes an array of blog post objects and a tag string. Returns a new array of blog posts where each post has the new tag added to its tags array. The original posts array should not be mutated.
+*/
+
 function addTagToPosts(posts, tag) {
     // My logic here
+let updatedPosts = posts.map((post) => {
+    let updatedPost = {
+      title: post.title, 
+      tags: [...post.tags, tag] 
+    };
+
+    return updatedPost;
+  });
+
+  return updatedPosts;
   }
   
   module.exports = addTagToPosts;
