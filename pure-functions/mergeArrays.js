c10d4a172f49d53aba578fbb726eeecf1d8513d3
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-18 19:01:02 +0700

    Adds logic for mergeArrays function

--- pure-functions/mergeArrays.js
+++ pure-functions/mergeArrays.js
@@ -1,5 +1,19 @@
-function mergeArrays (arr1, arr2) {
+function mergeArrays(arr1, arr2) {
+    let newArray = [];
 
+    for (let i = 0; i < arr1.length; i++) {
+        if (!newArray.includes(arr1[i])) {
+            newArray.push(arr1[i]);
+        }
+    }
+
+    for (let j = 0; j < arr2.length; j++) {
+        if (!newArray.includes(arr2[j])) {
+            newArray.push(arr2[j]);
+        }
+    }
+
+    return newArray;
 }
 
 module.exports = mergeArrays;
