df270751fd26c63352b02cfe70679eca845b5ad7
hannahchapman79 <hannahchapman1999@aol.com>
2025-01-20 11:14:58 +0700

    Adds logic to filter function

--- higher-order-functions/filter.js
+++ higher-order-functions/filter.js
@@ -1,5 +1,11 @@
 function filter(array, callbackfn) {
-
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callbackfn(array[i], i, array)) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray;
   }
 
   module.exports = filter;
